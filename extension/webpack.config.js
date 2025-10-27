8fbc6fe683a4544183c60a77e90c663646d683ce
nirantansaha709-beep <nirantansaha709@gmail.com>
2025-10-27 14:48:42 +0530

    Create webpack.config.js

---
+++ extension/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: "./dist",
+    port: 8080,
+    hot: true,
+  },
+};
