b54f496d7f8400152183e42843cef3b75e3ca2f8
nirantansaha709-beep <nirantansaha709@gmail.com>
2025-10-27 14:52:15 +0530

    Create Loader.tsx

---
+++ extension/src/components/Loader.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Loader = () => (
+  <div className="flex justify-center items-center my-2">
+    <div className="w-5 h-5 border-2 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+  </div>
+);
+
+export default Loader;
